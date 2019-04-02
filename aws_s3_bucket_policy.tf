225db5243c9a97f732d961e918ed574ad3315200
Richard Gutkowski <richard@ososolution.com>
2019-04-02 23:17:46 +0100

    Fixing snags

--- aws_s3_bucket_policy.tf
+++ aws_s3_bucket_policy.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = "${aws_s3_bucket.bucket.id}"
 
   policy = <<POLICY
-  {
+{
     "Version":"2012-10-17",
     "Statement": [
       {
@@ -20,6 +20,6 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
         }
       }
     ]
-  }
-  POLICY
+}
+POLICY
 }
