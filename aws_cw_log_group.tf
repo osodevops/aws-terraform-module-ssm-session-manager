fd4ec43e51773232597ee2391eb3545b5b8b1f8c
Sion Smith <sion.smith@gmail.com>
2020-01-20 17:31:53 +0000

    removed unused environment reference.

--- aws_cw_log_group.tf
+++ aws_cw_log_group.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "session-manager-cwl-group" {
-  name = "${var.cloudwatch_log_group_name}"
+  name = var.cloudwatch_log_group_name
 
   tags = "${merge(var.common_tags,
     map("Name", "${var.cloudwatch_log_group_name}")
