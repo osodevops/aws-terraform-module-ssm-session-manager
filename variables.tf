eb05f5ccadfb949a89899aa7dca4ac4fb363c71c
Richard Gutkowski <richard@ososolution.com>
2020-04-02 17:52:15 +0100

    minor adjustments

--- variables.tf
+++ variables.tf
@@ -1,42 +1,51 @@
 variable "kms_alias" {
+  type    = string
   default = ""
 }
 
 variable "s3_bucket_name" {
+  type    = string
   default = ""
 }
 
 variable "s3_bucket_acl" {
+  type    = string
   default = "private"
 }
 
 variable "s3_key_prefix" {
+  type    = string
   default = ""
 }
 
 variable "s3_encryption_enabled" {
+  type    = bool
   default = false
 }
 
 variable "s3_bucket_force_destroy" {
+  type    = bool
   default = false
 }
 
 variable "bucket_versioning" {
+  type    = bool
   default = false
 }
 
 variable "cloudwatch_log_group_name" {
+  type    = bool
   default = ""
 }
 
 variable "cloudwatch_encryption_enabled" {
+  type    = bool
   default = false
 }
 
 variable "common_tags" {
-  type = "map"
+  type    = map
   default = {
-    Name = "Oso-Test"
+    Name  = "Oso-Test"
   }
 }
